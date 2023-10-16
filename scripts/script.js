fc178fab7f0aa83c321008a9daf17bb58f6db773
Aurélien Plazzotta <aurelien.plazzotta@tutanota.com>
2023-10-16 11:37:34 +0200

    Add new template string with affectation operator

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,6 @@ let fname = "Aurèle";
 alert(`Here is our first pop-up with ${fname}!`);
 console.log(fname);
 // Adding a new comment.
+let age = 37;
+age += 1;
+console.log(`I am ${age}-year old.`);
